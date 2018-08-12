62839e136490743a0d7b279c4a8ac5b40a03d033
Adrian Lewis <zen_global@hotmail.com>
2018-08-12 19:25:38 +0100

    Extending the countdown schema to incorporate due and expiry and allow prefix text to be set by plugin

--- assets/js/wp-countdown.js
+++ assets/js/wp-countdown.js
@@ -1,56 +1,87 @@
+/*
+    wp-countdown.js
+
+    Expects DIV tag with the following data attributes;
+    if data-expiry-date is not set then this function is disabled (essentially allows two countdowns in one)
+
+    <div class='wp-countdown'
+         data-pending-due-text='Countdown until the start of Notting Hill Carnival'
+         data-pending-expiry-text='Countdown until the end of Notting Hill Carnival'
+         data-expired-text='Carnival has finished!'
+         data-due-date='2018-08-25T06:00'
+         data-expiry-date='2018-08-26T21:00'>
+    </div>
+*/
+
 window.countdown = window.countdown || {};
 
 window.countdown.UI = (function (jQuery) {
+    var countdownContainer;
+    var targetDate;
+
     Instance = (function () {
-        function initContainer(element) {
-            // Extract countdown date from data attribute
-            var dateTimeParts = element.data('date').split('T');
+        function getDateFromElement(element, dataAttributeName) {
+            var dateTimeParts = element.data(dataAttributeName).split('T');
             var dateParts = dateTimeParts[0].split('-');
             var timeParts = dateTimeParts[1].split(':');
-            var countdownDate = new Date(
+            return new Date(
                 parseInt(dateParts[0]),
                 parseInt(dateParts[1]) - 1,
                 parseInt(dateParts[2]) - 1,
                 parseInt(timeParts[0]),
                 parseInt(timeParts[1]))
                 .getTime();
+        }
+      
+        function getTimeStringFromDistance(distance) {
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            
+            var timeString = '';
+            if (days > 0) {
+                timeString += days + 'd ';
+            }
+            if (days > 0 || hours > 0) {
+                timeString += hours + 'h ';
+            }
+            if (days > 0 || hours > 0 || minutes > 0) {
+                timeString += minutes + 'm ';
+            }
+            if (days > 0 || hours > 0 || minutes > 0 || seconds > 0) {
+                timeString += seconds + 's';
+            }
+            return timeString;
+        }
+      
+        function initContainer(element) {
+            // Extract countdown date from data attribute
+            var dueDate = getDateFromElement(element, 'dueDate');
+            var expiryDate = getDateFromElement(element, 'expiryDate');
 
             // Update the count down every 1 second
             var timer = setInterval(function() {
                 // Get todays date and time
                 var now = new Date().getTime();
                 
-                // Find the distance between now and the count down date
-                var distance = countdownDate - now;
-                
-                // Time calculations for days, hours, minutes and seconds
-                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-                
+                var prefixString = '';
                 var timeString = '';
-                if (days > 0) {
-                    timeString += days + 'd ';
+                if (dueDate > now) {
+                    prefixString = element.data('pendingDueText');
+                    timeString = getTimeStringFromDistance(dueDate - now);
                 }
-                if (days > 0 || hours > 0) {
-                    timeString += hours + 'h ';
+                else if (expiryDate > now) {
+                    prefixString = element.data('pendingExpiryText');
+                    timeString = getTimeStringFromDistance(expiryDate - now);
                 }
-                if (days > 0 || hours > 0 || minutes > 0) {
-                    timeString += minutes + 'm ';
-                }
-                if (days > 0 || hours > 0 || minutes > 0 || seconds > 0) {
-                    timeString += seconds + 's';
+                else {
+                    prefixString = element.data('expiryText');
+                    clearInterval(timer);
                 }
 
                 // Display the result
-                element.html('<span class=\'active\'>' + timeString + '</span');
-                
-                // If the count down is finished, write some text
-                if (distance < 0) {
-                    clearInterval(timer);
-                    element.html('<span class=\'expired\'>Is here!</span>');
-                }
+                element.html('<span class=\'prefix\'>' + prefixString + '</span><span class=\'time\'>' + timeString + '</span');
             }, 1000);
         }
         
@@ -60,16 +91,16 @@ window.countdown.UI = (function (jQuery) {
     });
 
     function initCountdown() {
-        jQuery('.wp-countdown').each(
+        $('.wp-countdown').each(
             function (index, element) {
                 var instance = new Instance();
-                instance.init(jQuery(element));
+                instance.init($(element));
             });
     }
 
     return {
         init: initCountdown
     };
-})(jQuery);
+})($);
 
 window.countdown.UI.init();
