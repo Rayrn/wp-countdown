ac414d83a0724cef52b39325e74e95108a845292
Adrian Lewis <zen_global@hotmail.com>
2018-08-12 14:55:50 +0100

    Updated JS

---
+++ assets/js/wp-countdown.js
@@ -0,0 +1,74 @@
+window.countdown = window.countdown || {};
+
+window.countdown.UI = (function (jQuery) {
+    var countdownContainer;
+    var targetDate;
+
+    Instance = (function () {
+        function initContainer(element) {
+            // Extract countdown date from data attribute
+            var countdownDateParts = element.data('date').split('-');
+            var countdownDate = new Date(
+                parseInt(countdownDateParts[0]),
+                parseInt(countdownDateParts[1]),
+                parseInt(countdownDateParts[2]))
+                .getTime();
+
+            // Update the count down every 1 second
+            var timer = setInterval(function() {
+                // Get todays date and time
+                var now = new Date().getTime();
+                
+                // Find the distance between now and the count down date
+                var distance = countdownDate - now;
+                
+                // Time calculations for days, hours, minutes and seconds
+                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                
+                var timeString = '';
+                if (days > 0) {
+                    timeString += days + 'd ';
+                }
+                if (days > 0 || hours > 0) {
+                    timeString += hours + 'h ';
+                }
+                if (days > 0 || hours > 0 || minutes > 0) {
+                    timeString += minutes + 'm ';
+                }
+                if (days > 0 || hours > 0 || minutes > 0 || seconds > 0) {
+                    timeString += seconds + 's';
+                }
+
+                // Display the result
+                element.html('<span class=\'active\'>' + timeString + '</span');
+                
+                // If the count down is finished, write some text
+                if (distance < 0) {
+                    clearInterval(timer);
+                    element.html('<span class=\'expired\'>Is here!</span>');
+                }
+            }, 1000);
+        }
+        
+        return {
+            init: initContainer
+        };
+    });
+
+    function initCountdown() {
+        $('.wp-countdown').each(
+            function (index, element) {
+                var instance = new Instance();
+                instance.init($(element));
+            });
+    }
+
+    return {
+        init: initCountdown
+    };
+})($);
+
+window.countdown.UI.init();
